d50d07717e11cf8884acc99ed075a9113752d8b3
Dawid Wajda <dawid888@mail.com>
2022-02-28 14:46:00 +0100

    package: update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,9 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import Image from 'next/image';
 
 const Home: NextPage = () => {
   return (
-    <div className='align-center flex h-screen w-screen justify-center bg-slate-600 text-slate-50'>
+    <div className='align-center flex h-screen w-screen justify-center bg-black text-white'>
       <Head>
         <title>Hi</title>
         <meta name='description' content='Hello in the template' />
@@ -13,7 +12,6 @@ const Home: NextPage = () => {
 
       <main className='flex flex-col justify-center'>
         <h1 className='text-center'>Hi</h1>
-        <Image src='/vercel.svg' width={100} height={100} alt='vercel' />
       </main>
     </div>
   );
